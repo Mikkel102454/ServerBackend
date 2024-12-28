6496802f139a33d98d2a290086de6c9784813637
mikkel102454 <mikkeliversen09@outlook.dk>
2024-12-29 00:44:08 +0100

    d

--- ServerFiles/Minecraft/1.21.4/files/start.sh
+++ ServerFiles/Minecraft/1.21.4/files/start.sh
@@ -3,4 +3,6 @@ javaInstallLocation="/usr/lib/jvm/java-${javaRequired}-openjdk-amd64/bin/java"
 
 maxRam="2G"
 "${javaInstallLocation}" --version
+"${javaInstallLocation}" -XshowSettings:properties -version 2>&1 | grep java.security
+
 #"${javaInstallLocation}" -Xmx"${maxRam}" -Xms"${maxRam}" -jar server.jar nogui
