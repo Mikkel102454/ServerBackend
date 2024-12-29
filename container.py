34954df354c7beaaf13b3f3f18411f50ce099cbf
mikkel102454 <mikkeliversen09@outlook.dk>
2024-12-29 01:54:18 +0100

    d

--- container.py
+++ container.py
@@ -14,28 +14,39 @@ def start_container(serverID, ram, java, port):
                 hostPath: {'bind': hostPath, 'mode': 'rw'}
             },
         if(java != 0):
-            javaCerts = f"etc/ssl/certs/java"
-            javaPath1 = f"/usr/lib/jvm/java-{java}-openjdk-amd64"
-            javaPath2 = f"/etc/java-{java}-openjdk"
-            tempVolumes={
-                hostPath: {'bind': hostPath, 'mode': 'rw'},
-                javaCerts: {'bind': javaCerts, 'mode': 'ro'},
-                javaPath1: {'bind': javaPath1, 'mode': 'ro'},
-                javaPath2: {'bind': javaPath2, 'mode': 'ro'}
-            }
-        
-        # Create a container with memory limits
-        container = client.containers.run(
-            "ubuntu:24.04",  # Base image
-            name=serverID,
-            command=f"sh -c 'cd {hostPath} && sh container.sh'",
-            volumes=tempVolumes,
-            detach=True,
-            ports={
-                '25565/tcp': ('0.0.0.0', port)  # Map container port 25565 to host port 25565
-            },
-            mem_limit=ram
-        )
+            # USES JAVA
+            container = client.containers.run(
+                "ubuntu:24.04",  # Base image
+                name=serverID,
+                command=f"sh -c 'apt update && apt install -y openjdk-{java}-jdk && export JAVA_HOME=/usr/lib/jvm/java-{java}-openjdk-amd64 && export PATH=$JAVA_HOME/bin:$PATH && cd /host_path && java -version && sh container.sh'",
+                volumes= {
+                    hostPath: {'bind': '/host_path', 'mode': 'rw'}
+                },
+                environment={
+                    "JAVA_HOME": f"/usr/lib/jvm/java-{java}-openjdk-amd64",
+                    "PATH": f"/usr/lib/jvm/java-{java}-openjdk-amd64/bin:" + "${PATH}"
+                },
+                detach=True,
+                ports={
+                    '25565/tcp': ('0.0.0.0', port)  # Map container port 25565 to host port 25565
+                },
+                mem_limit=ram
+            )
+        else:
+            # DOSNT USE JAVA
+            container = client.containers.run(
+                "ubuntu:24.04",  # Base image
+                name=serverID,
+                command=f"sh -c 'cd {hostPath} && sh container.sh'",
+                volumes= {
+                    hostPath: {'bind': '/host_path', 'mode': 'rw'}
+                },
+                detach=True,
+                ports={
+                    '25565/tcp': ('0.0.0.0', port)  # Map container port 25565 to host port 25565
+                },
+                mem_limit=ram
+            )
         activeContainers.append(container)
         print(serverID)
     except Exception as e:
