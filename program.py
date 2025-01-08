985469440629c0de328d7e13a279b81611dc9420
mikkel102454 <mikkeliversen09@outlook.dk>
2025-01-08 08:47:51 +0100

    f

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ from container import get_all_containers
 from container import stop_container
 from database import connect_to_database
 from database import create_table
-connect_to_database("root", "")
+connect_to_database("mikkel", "Bredsten7182")
 create_table("mc-servers", "uuid VARCHAR(255), version VARCHAR(255), javaVersion VARCHAR(255), name VARCHAR(255), ram VARCHAR(255)")
 
 while True:
@@ -14,18 +14,18 @@ while True:
     if(_cmd == "list"):
         #List all servers from database
         get_all_containers()
-    if(_cmd == "create"):
+    elif(_cmd == "create"):
         _name = input("name:")
         _v = input("version:")
         create_new_server(_name, _v)
-    if(_cmd == "start"):
+    elif(_cmd == "start"):
         _id = input("id:")
         _port = input("port:")
         start_server(_id, _port)
-    if(_cmd == "remove"):
+    elif(_cmd == "remove"):
         _id = input("id:")
         stop_container(_id)
-    if(_cmd == "help"):
+    elif(_cmd == "help"):
         print("list | create | remove")
 
 
