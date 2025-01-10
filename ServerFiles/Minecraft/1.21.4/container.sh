9b994a4f596b571ba17fd3face405de068a8d763
mikkel102454 <mikkeliversen09@outlook.dk>
2025-01-10 09:13:28 +0100

    d

--- ServerFiles/Minecraft/1.21.4/container.sh
+++ ServerFiles/Minecraft/1.21.4/container.sh
@@ -1,10 +1,11 @@
 #Start Minecraft Server
 apt update 
 apt install -y openjdk-21-jdk
+apt install git
+apt install make
 git clone https://github.com/Tiiffi/mcrcon.git
 cd mcrcon
-make
-sudo make install
+make install
 cd ..
 cd files
 sh start.sh
